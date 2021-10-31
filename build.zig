4c993a2a603819ffbeb7339df27fb2f849bf6d13
vole-dev <dev@vole.dev>
2021-10-31 22:47:22 +0000

    run qemu from build.zig, refactoring

--- build.zig
+++ build.zig
@@ -15,47 +15,49 @@ pub fn build(b: *std.build.Builder) void {
         .abi = .none,
     };
 
-    const exe = b.addExecutable("kernel", null);
-    exe.setOutputDir("kernel");
+    const build_kernel = b.addExecutable("kernel", null);
+    build_kernel.setOutputDir("kernel");
 
-    exe.setLinkerScriptPath(.{ .path = "kernel/kernel.ld" });
+    build_kernel.setLinkerScriptPath(.{ .path = "kernel/kernel.ld" });
     const cflags = &[_][]const u8{ "-Wall", "-Werror" };
-    exe.addIncludeDir(".");
-    exe.addAssemblyFile("kernel/entry.S");
-    exe.addCSourceFile("kernel/start.c", cflags);
-    exe.addCSourceFile("kernel/console.c", cflags);
-    exe.addCSourceFile("kernel/printf.c", cflags);
-    exe.addCSourceFile("kernel/uart.c", cflags);
-    exe.addCSourceFile("kernel/kalloc.c", cflags);
-    exe.addCSourceFile("kernel/spinlock.c", cflags);
-    exe.addCSourceFile("kernel/string.c", cflags);
-    exe.addCSourceFile("kernel/main.c", cflags);
-    exe.addCSourceFile("kernel/vm.c", cflags);
-    exe.addCSourceFile("kernel/proc.c", cflags);
-    exe.addAssemblyFile("kernel/swtch.S");
-    exe.addAssemblyFile("kernel/trampoline.S");
-    exe.addCSourceFile("kernel/trap.c", cflags);
-    exe.addCSourceFile("kernel/syscall.c", cflags);
-    exe.addCSourceFile("kernel/sysproc.c", cflags);
-    exe.addCSourceFile("kernel/bio.c", cflags);
-    exe.addCSourceFile("kernel/fs.c", cflags);
-    exe.addCSourceFile("kernel/log.c", cflags);
-    exe.addCSourceFile("kernel/sleeplock.c", cflags);
-    exe.addCSourceFile("kernel/file.c", cflags);
-    exe.addCSourceFile("kernel/pipe.c", cflags);
-    exe.addCSourceFile("kernel/exec.c", cflags);
-    exe.addCSourceFile("kernel/sysfile.c", cflags);
-    exe.addAssemblyFile("kernel/kernelvec.S");
-    exe.addCSourceFile("kernel/plic.c", cflags);
-    exe.addCSourceFile("kernel/virtio_disk.c", cflags);
-    exe.setTarget(target);
-    exe.target_abi = .lp64d;
-    exe.code_model = .medium;
-    exe.pie = false;
-    exe.setBuildMode(mode);
-    exe.install();
+    build_kernel.addIncludeDir(".");
+    build_kernel.addAssemblyFile("kernel/entry.S");
+    build_kernel.addCSourceFile("kernel/start.c", cflags);
+    build_kernel.addCSourceFile("kernel/console.c", cflags);
+    build_kernel.addCSourceFile("kernel/printf.c", cflags);
+    build_kernel.addCSourceFile("kernel/uart.c", cflags);
+    build_kernel.addCSourceFile("kernel/kalloc.c", cflags);
+    build_kernel.addCSourceFile("kernel/spinlock.c", cflags);
+    build_kernel.addCSourceFile("kernel/string.c", cflags);
+    build_kernel.addCSourceFile("kernel/main.c", cflags);
+    build_kernel.addCSourceFile("kernel/vm.c", cflags);
+    build_kernel.addCSourceFile("kernel/proc.c", cflags);
+    build_kernel.addAssemblyFile("kernel/swtch.S");
+    build_kernel.addAssemblyFile("kernel/trampoline.S");
+    build_kernel.addCSourceFile("kernel/trap.c", cflags);
+    build_kernel.addCSourceFile("kernel/syscall.c", cflags);
+    build_kernel.addCSourceFile("kernel/sysproc.c", cflags);
+    build_kernel.addCSourceFile("kernel/bio.c", cflags);
+    build_kernel.addCSourceFile("kernel/fs.c", cflags);
+    build_kernel.addCSourceFile("kernel/log.c", cflags);
+    build_kernel.addCSourceFile("kernel/sleeplock.c", cflags);
+    build_kernel.addCSourceFile("kernel/file.c", cflags);
+    build_kernel.addCSourceFile("kernel/pipe.c", cflags);
+    build_kernel.addCSourceFile("kernel/exec.c", cflags);
+    build_kernel.addCSourceFile("kernel/sysfile.c", cflags);
+    build_kernel.addAssemblyFile("kernel/kernelvec.S");
+    build_kernel.addCSourceFile("kernel/plic.c", cflags);
+    build_kernel.addCSourceFile("kernel/virtio_disk.c", cflags);
+    build_kernel.setTarget(target);
+    build_kernel.target_abi = .lp64d;
+    build_kernel.code_model = .medium;
+    build_kernel.pie = false;
+    build_kernel.setBuildMode(mode);
 
-    const mkfs = b.step("mkfs", "Build mkfs executable");
+    const kernel = b.step("kernel", "Build xv6 kernel");
+    kernel.dependOn(&build_kernel.step);
+    b.default_step = kernel;
+
     const build_mkfs = b.addExecutable("mkfs", null);
     build_mkfs.setOutputDir("mkfs");
     build_mkfs.addIncludeDir(".");
@@ -63,6 +65,8 @@ pub fn build(b: *std.build.Builder) void {
     build_mkfs.setBuildMode(.Debug);
     build_mkfs.linkLibC();
     build_mkfs.setTarget(b.standardTargetOptions(.{}));
+
+    const mkfs = b.step("mkfs", "Build mkfs executable");
     mkfs.dependOn(&build_mkfs.step);
 
     const UPROGS = [_][]const u8{
@@ -83,20 +87,24 @@ pub fn build(b: *std.build.Builder) void {
         "user/_wc",
         "user/_zombie",
     };
-    const fs_img = b.step("fs.img", "Create fs.img");
     var build_fs_img = build_mkfs.run();
     build_fs_img.addArgs(&[_][]const u8{ "fs.img", "README" } ++ UPROGS);
-    fs_img.dependOn(&build_fs_img.step);
-    build_fs_img.step.dependOn(&exe.step);
+    build_fs_img.step.dependOn(&build_kernel.step);
 
-    const qemu = b.step("qemu", "Run the OS in qemu");
+    const fs_img = b.step("fs.img", "Create fs.img");
+    fs_img.dependOn(&build_fs_img.step);
+
+    const num_cpus = "3";
     var qemu_args = &[_][]const u8{
-        "qemu-system-x86_64",
-        "-kernel",
-        //exe.getOutputSource().getPath(b),
-        "-curses",
+        "qemu-system-riscv64",
+        "-machine","virt","-bios","none","-kernel","kernel/kernel","-m","128M","-smp",num_cpus,"-nographic",
+        "-drive","file=fs.img,if=none,format=raw,id=x0",
+        "-device","virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0"
     };
     const run_qemu = b.addSystemCommand(qemu_args);
+    run_qemu.step.dependOn(&build_kernel.step);
+    run_qemu.step.dependOn(&build_fs_img.step);
+
+    const qemu = b.step("qemu", "Run the OS in qemu");
     qemu.dependOn(&run_qemu.step);
-    run_qemu.step.dependOn(&exe.step);
 }
